21157aecf8e83a786229fa92bfc61dc6132591a3
Nika.P <pns9595@mail.ru>
2020-09-24 21:55:04 +0300

    Добавьте файлы проекта.

---
+++ Tests/ConsoleTests/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace ConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var from = new MailAddress("pns95@mail.ru", "Павел");
+            var to = new MailAddress("pns9595@mail.ru", "Павел");
+            
+
+            var message = new MailMessage(from, to);
+            //var msg = new MailAddress("user@server.ru", "qwe@ASD.ru");
+
+            message.Subject = "Заголовок письма от " + DateTime.Now;
+            message.Body = "Текст тестового письма + " + DateTime.Now;
+
+            var client = new SmtpClient("smtp.mail.ru", 25);
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "",
+                Password = ""
+            };
+
+            client.Send(message);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tests/WPFTests/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows;
+
+namespace WPFTests
+{
+    public partial class MainWindow
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void OnSendButtonClick(object sender, RoutedEventArgs e)
+        {
+            var from = new MailAddress("pns95@mail.ru", "Ника");
+            var to = new MailAddress("pns9595@mail.ru", "ник");
+            
+
+            var message = new MailMessage(from, to);
+
+            message.Subject = "Заголовок письма от " + DateTime.Now;
+            message.Body = "Текст тестового письма + " + DateTime.Now;
+
+            var client = new SmtpClient("smtp.mail.ru", 25);
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential
+            {
+                UserName = LoginEdit.Text,
+                SecurePassword = PasswordEdit.SecurePassword
+            };
+
+            client.Send(message);
+
+        }
+    }
+}
